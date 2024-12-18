9fc0acdfd95dc64cb5793a1e81a40a50d26102ac
TechWebSid <siddharthasrivastava30@gmail.com>
2024-12-18 14:16:21 +0530

    captain authentication completed

--- backend/controllers/captain.controller.js
+++ backend/controllers/captain.controller.js
@@ -1,6 +1,7 @@
 const captainModel = require('../models/captain.model')
 const captainService = require('../services/captain.service')
 const {validationResult} = require('express-validator')
+const blackListTokenModel = require('../models/blacklistToken.model')
 
 module.exports.registerCaptain = async (req,res,next)=>{
 
@@ -33,4 +34,43 @@ module.exports.registerCaptain = async (req,res,next)=>{
 
     res.status(201).json({captain,token})
 
+
+    
+
+}
+
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const captain = await captainModel.findOne({ email }).select('+password');
+    if (!captain) {
+        return res.status(400).json({ message: "Captain not found" });
+    }
+
+    const isPasswordCorrect = await captain.comparePassword(password);
+    if (!isPasswordCorrect) {
+        return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = captain.generateAuthToken();
+    res.cookie("token", token);
+    res.status(200).json({ message: "Login successful", captain, token });
+}
+
+
+module.exports.getCaptainProfile = async (req,res,next)=>{
+    const captain = req.captain;
+    res.status(200).json({captain})
+}
+
+module.exports.logoutCaptain = async (req,res,next)=>{
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+    await blackListTokenModel.create({token:token});
+    res.clearCookie("token");
+    res.status(200).json({message:"Logout successful"})
 }
--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const captainController = require('../controllers/captain.controller')
 const {body} = require('express-validator')
 const router = express.Router()
+const userMiddleware = require('../middlewares/user.middleware')
 
 
 router.post("/register", [
@@ -14,5 +15,15 @@ router.post("/register", [
     body("vehicle.capacity").isInt({min:1}).withMessage("Capacity must be at least 1"),
     body("vehicle.vehicleType").isIn(["car","motorcycle","auto"]).withMessage("Invalid vehicle type"),
 ],captainController.registerCaptain)
+
+
+router.post("/login", [
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password").isLength({min:6}).withMessage("Password must be at least 6 characters long"),
+],captainController.loginCaptain)
+
+router.get("/profile",userMiddleware.authCaptain,captainController.getCaptainProfile)
+
+router.post("/logout",userMiddleware.authCaptain,captainController.logoutCaptain)
 
 module.exports = router;
